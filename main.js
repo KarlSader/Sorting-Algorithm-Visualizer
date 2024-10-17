44d08219139a24b5049c030993f50bb3c5bb3501
KarlSader <karl.sader@gmail.com>
2024-10-17 16:51:22 -0400

    Creating List of Squares
    Adding the ability for the user to create a list and create squares of different sizes from the given list input

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-function resizeSquare() {
-    const size = document.getElementById("sizeInput").value;
-    const square = document.getElementById("square");
-    console.log(size)
-    square.style.width = size + "px";
-    square.style.height = size + "px";
+let list = []
+
+function addSquare() {
+    // Get the value of the input
+    const size = document.getElementById("listInput").value
+    // Add the number to a list
+    list.push(size)
+    // Clear the input field
+    document.getElementById("listInput").value=null
+}
+
+function doneSquare() {
+    console.log(list)
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i])
+    }
+    // Get square container
+    const container = document.getElementById("squareContainer")
+    // Clear existing squares
+    container.innerHTML = ""
+
+    for (let i = 0; i < list.length; i++) {
+        const square = document.createElement("div")
+        square.className = "square"
+        square.style.width = 30 + "px"
+        square.style.height = list[i] + "px"
+        container.appendChild(square)
+    }
+
+    // Clear the list
+    list = []
 }
